c1bdc697a670021907783aae95d77187bd4f1097
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-10 15:23:05 +0200

    creation

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang='fr'>
+	<head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>PHP ex 5.10 tableau</title>
+    </head>
+    <body >
+        <!--
+        /*  Faire une page index.php. 
+            Tester sur cette page que tous les paramètres de cette URL existent et les afficher : 
+                index.php?lastname=Nemare&firstname=Jean
+        */
+        -->
+        <p>Veuillez vous identifier</p>
+        <form action="test_url.php" methode="get">
+            <label for="lastname">Nom :</label>
+			<input type="string" name="lastname" placeholder="nom" required>
+            <label for="firstname">Prénom :</label>
+			<input type="string" name="firstname" placeholder="prénom" required>
+            <input type="submit" value="OK">
+        </form>
+    </body>
+</html>
---
+++ test_url.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang='fr'>
+	<head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>PHP ex 5.10 tableau</title>
+    </head>
+    <body >
+        <?php
+        $lastName=$_GET["lastname"];
+        $firstName=$_GET["firstname"];
+        ?>
+        <p>test_url.php?lastname=<?= $lastName ?>&firstname=<?= $firstName ?></p>
+    </body>
+</html>
