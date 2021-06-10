89a6b2102624f3b68945d81382935a6469195dff
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-10 15:42:51 +0200

    evo2

--- test_url.php
+++ test_url.php
@@ -3,14 +3,24 @@
 	<head>
         <meta charset='UTF-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <title>PHP ex 5.10 tableau</title>
+        <title>PHP ex 6.1 URL Server</title>
     </head>
     <body>
-        <p>Should receive parameters with the GET method</p>
         <?php
-            $lastName=$_GET["lastname"];
-            $firstName=$_GET["firstname"];
-        ?>
+        if ($_SERVER['REQUEST_METHOD'] === 'POST')
+        {
+            $lastName=$_POST['lastname'];
+            $firstName=$_POST['firstname'];
+            $usedMethod='POST';
+        }
+        else
+        {
+            $usedMethod='GET';
+            $lastName=$_GET['lastname'];
+            $firstName=$_GET['firstname'];
+        }
+       ?>
+        <p>Should receive parameters with the <?= $usedMethod ?> method</p>
         <p>test_url.php?lastname=<?= $lastName ?>&firstname=<?= $firstName ?></p>
     </body>
 </html>
